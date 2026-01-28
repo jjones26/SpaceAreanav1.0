19f2674a8ddc9e7be7f49e44038048dac84814ef
Jacob Jones <keneast24@gmail.com>
2026-01-28 16:10:07 -0500

    Added moving platforms

--- objects/movingplatformvertical.gd
+++ objects/movingplatformvertical.gd
@@ -1,11 +1,16 @@
 extends AnimatableBody3D
 
-@export var distance = Vector3(0, 10, 0) # How far to move
-@export var speed = 1.0
+@export var movement_vector = Vector3(0, 5, 0) # Direction and distance
+@export var speed = 2.0
+
 var time = 0.0
+@onready var start_position = position # Captures the position you set in the editor
 
 func _physics_process(delta):
 	time += delta * speed
-	# Use a sine wave to move back and forth smoothly
-	var offset = (sin(time) + 1.0) / 2.0 
-	position = distance * offset
+	
+	# Calculate the "swing" (0 to 1)
+	var factor = (sin(time) + 1.0) / 2.0 
+	
+	# Move relative to the starting position
+	position = start_position + (movement_vector * factor)
